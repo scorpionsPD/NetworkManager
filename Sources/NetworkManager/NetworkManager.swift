fa0135a197920a3572434f20b6dfa96d2e864dc1
Pradeep Dahiya <pradeep.dahiya@crmnext.com>
2023-05-04 17:15:00 +0100

    Initial Commit

---
+++ Sources/NetworkManager/NetworkManager.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+class NetworkManager {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func fetch<T: Decodable>(url: URL, completion: @escaping (Result<T>) -> Void) {
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data returned", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(T.self, from: data)
+                completion(.success(result))
+            } catch let decodingError {
+                completion(.failure(decodingError))
+            }
+        }
+        task.resume()
+    }
+}
