1a7ac0ca58096409be46cbf109bb12a462ab242c
Pradeep Dahiya <pradeepdahiya@businessnext.com>
2023-05-25 13:17:00 +0100

    updates

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -1,41 +1,41 @@
 import Foundation
 
-public enum Result<T> {
-    case success(T)
-    case failure(Error)
+public enum NetworkError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case invalidResponse
+    // Add more error cases as needed
 }
 
-public class NetworkManager {
-    
-    private let session: URLSession
-    
-    init(session: URLSession = URLSession.shared) {
-        self.session = session
-    }
-    
-    public static func fetch<T: Decodable>(url: URL, completion: @escaping (Result<T>) -> Void) {
-        
-        let session: URLSession = URLSession()
-        
-        let task = session.dataTask(with: url) { (data, response, error) in
+public final class NetworkService {
+    public static let shared = NetworkService()
+
+    private init() {}
+
+    public func request<T: Decodable>(urlString: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
-                completion(.failure(error))
+                completion(.failure(.requestFailed(error)))
                 return
             }
-            
+
             guard let data = data else {
-                completion(.failure(NSError(domain: "No data returned", code: 0, userInfo: nil)))
+                completion(.failure(.invalidResponse))
                 return
             }
-            
+
             do {
                 let decoder = JSONDecoder()
                 let result = try decoder.decode(T.self, from: data)
                 completion(.success(result))
-            } catch let decodingError {
-                completion(.failure(decodingError))
+            } catch {
+                completion(.failure(.invalidResponse))
             }
-        }
-        task.resume()
+        }.resume()
     }
 }
